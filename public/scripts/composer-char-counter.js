bdf9fa4111ddcff12c6595acc7f75ab78a00f188
Kevin Lee <kevin.skyulee@gmail.com>
2020-09-08 15:01:49 -0700

    added counter logic with jquery

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+  const tweetInput = $('#tweet-text');
+
+  tweetInput.keyup(function() {
+    const textLength = $(this).val().length;
+    const counter = $(this).parent('form').children('footer').children('.counter');
+    counter.html(140 - textLength); 
+
+    if (textLength > 140) {
+      counter.addClass("counter-red");
+    } else {
+      counter.removeClass("counter-red");
+    }
+  });
+});
+
