6befb9687ba3e49c7d1dc3ad5ba50589d630a38e
Kevin Lee <kevin.skyulee@gmail.com>
2020-09-09 14:32:40 -0700

    added get method to load tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -34,33 +34,15 @@ $(() => {
     }
   };
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  const loadtweets = () => {
+    $.get('./tweets')
+      .then(res => {
+        renderTweets(res);
+      });
+  };
 
   const $tweetForm= $(".new-tweet").children('form');
-
+  
   //Event listener for new tweet form
   $tweetForm.on('submit', function (event) {
     //Prevent page refresh
@@ -69,15 +51,12 @@ $(() => {
     //Serialize data to get key values 
     const serializedData = $(this).serialize();
 
+    //Post method to /tweets directory sending the serialized Data
     $.post('./tweets', serializedData)
-    .then((res) => {
-      console.log(res);
-    })  
-
+    .then(res => console.log(res));  
   })
 
-
-  renderTweets(data);
+  loadtweets();
 
 
 });
