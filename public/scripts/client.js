95253befd32bee13f699dc2e415e24f6e9934beb
Kevin Lee <kevin.skyulee@gmail.com>
2020-09-09 13:58:05 -0700

    added createTweetElement and renderTweetEleement functionality using jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,64 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(() => {
+  const createTweetElement = (tweetData) => {
+  const article = $('<article class="tweet">');
+  const header = $('<header>')
+                    .append($(`<img class="tweet-profile" src=${tweetData.user.avatars}>`))
+                    .append($(`<span class="name">`).text(tweetData.user.name))
+                    .append($(`<span class="username>`).text(tweetData.user.handle))
+
+  const tweetParagrah = $(`<p class="tweet-text">`).text(tweetData.content.text)
+  const footer = $('<footer>')
+                    .append($(`<span class="tweet-day">`).text(moment(tweetData.created_at).fromNow()))
+                    .append($('<img class="icon" src="./images/favorite.png">'))
+                    .append($('<img class="icon" src="./images/share.png>'))
+                    .append($('<img class="icon" src="./images/bookmark.png">'))
+
+  return article.append(header).append(tweetParagrah).append(footer);
+  }
+
+  const renderTweets = (data) => {
+    const $tweetContainer = $('#tweets-container');
+    for (const id in data) {
+      const tweet = data[id];
+
+      const tweetHTML = createTweetElement(tweet)
+      $tweetContainer.append(tweetHTML)
+    }
+  };
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  renderTweets(data);
+  
+  // const $tweet = createTweetElement(tweetData)
+
+  // Test / driver code (temporary)
+  // console.log($tweet); // to see what it looks like
+  // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
-  const tweetInput = $('#tweet-text');
+  const $tweetInput = $('#tweet-text');
 
-  tweetInput.keyup(function() {
+  $tweetInput.keyup(function() {
     const textLength = $(this).val().length;
     const counter = $(this).parent('form').children('footer').children('.counter');
     counter.html(140 - textLength); 
