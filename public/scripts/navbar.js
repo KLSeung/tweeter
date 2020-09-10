66cebfbd4ec61b53510c648adf33f7a53a3b54df
Kevin Lee <kevin.skyulee@gmail.com>
2020-09-09 21:45:20 -0700

    added scroll up button functionality

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -3,8 +3,10 @@ $(document).ready(() => {
     var position = $(this).scrollTop();
     if (position >= 180) {
       $('nav').addClass('shrink-navbar');
+      $('.scroll-up-button').css('opacity', '1')
     } else if (position <= 50) {
       $('nav').removeClass('shrink-navbar');
+      $('.scroll-up-button').css('opacity', '0')
     }
   });
 
@@ -20,5 +22,10 @@ $(document).ready(() => {
   $('.arrow-down-icon').on('click', () => {
     //Seems like this is easier than using the jquery animate method
     document.getElementById('new-tweet').scrollIntoView(true);
+  })
+
+  $('.scroll-up-button').on('click', () => {
+    //Seems like this is easier than using the jquery animate method
+    $(window).scrollTop(0);
   })
 });
